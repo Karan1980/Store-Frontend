10bd6dec139c6afc890b59cc799e9b02003f372f
karan <foxboroinstrument@gmail.com>
2024-09-01 22:13:59 +0530

    Initilization

---
+++ index.js
@@ -0,0 +1,16 @@
+const Express = require('express');
+const cors = require('cors');
+const app = Express();
+const PORT = 5000;
+app.use(cors())
+
+app.get('/', (req, res) => {
+    res.json({status: 'Successfull'})
+})
+
+app.listen(PORT, () => {
+  console.log("Backend Started at Port", PORT);
+  
+})
+
+
