71d5a512cdd56e17ea7b681619084a8041a4075f
keshav9761 <keshavtomar53@gmail.com>
2024-09-09 10:28:05 -0400

    first cicid test

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const Express = require('express');
 const cors = require('cors');
+const path = require('path');
 const app = Express();
 const PORT = 5000;
 app.use(cors())
-
-app.get('/', (req, res) => {
-    res.json({status: 'successfull'})
+app.use(Express.static(path.join(__dirname, 'Frontend')));
+app.get('**', (req, res) => {
+  res.sendFile(path.join(__dirname, 'Frontend', 'index.html'));
 })
 
 app.listen(PORT, () => {
